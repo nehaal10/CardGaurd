4c2cd23c01e7de33443a51d887e5abe2310b875d
Nehaal Reddy <nehaalreddy@Nehaals-MacBook-Pro.local>
2023-08-21 23:43:54 +0530

    email validation

--- helper/create_table.go
+++ helper/create_table.go
@@ -1,6 +1,9 @@
 package helper
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/nehaal10/CardGaurd/models"
 	"gorm.io/gorm"
 )
@@ -12,5 +15,16 @@ func CreateTable(db *gorm.DB) {
 	//if not create a table
 	if !isThereFraud && !isThereUser {
 		db.Migrator().CreateTable(&models.FraudDataBase{}, &models.UserDatabase{})
+	}
+
+	// this all will moved to another file
+	// we will get the data from spi calls
+	user := models.UserDatabase{UserName: "Nehaal", EmailID: "nehaal-gmail.com"}
+	msg := user.ValidateEmail()
+	if !msg {
+		log.Fatal(msg)
+		fmt.Println("In Valid Email ID")
+	} else {
+		db.Create(&user)
 	}
 }
--- models/models.go
+++ models/models.go
@@ -1,5 +1,11 @@
 package models
 
+import (
+	"net/mail"
+
+	"github.com/nehaal10/CardGaurd/utils"
+)
+
 type FraudDataBase struct {
 	ID         int64 `gorm:"primaryKey"`
 	CreditCard string
@@ -8,4 +14,11 @@ type FraudDataBase struct {
 type UserDatabase struct {
 	ID       uint `gorm:"primaryKey"`
 	UserName string
+	EmailID  string
+}
+
+func (u *UserDatabase) ValidateEmail() bool {
+	_, err := mail.ParseAddress(u.EmailID)
+	isVal := utils.CheckErrReturn(err)
+	return isVal
 }
